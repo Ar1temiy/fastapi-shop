a00c6b4972205a3bebf168e4655f42e21decb3f2
Ar1temiy <ar1temiy@gmail.com>
2026-02-09 14:43:53 +0200

    add models

---
+++ fastapi-shop/backend/app/models/category.py
@@ -0,0 +1,18 @@
+from datetime import datetime, date
+from typing import List
+from sqlalchemy import String, ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from database import Base
+
+
+class Habit(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), index=True, unique=True, nullable=False)
+    slug: Mapped[str] = mapped_column(String(100), index=True, unique=True, nullable=False)
+
+    products = relationship("Product", back_populates="category")
+    
+    def __repr__(self):
+        return f"<Category(id={self.id}, name='{self.name}')"
